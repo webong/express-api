11275186fb4e39bee963691cd8c39baba4ac016f
Wisdom <wizitendo10@gmail.com>
2017-12-17 16:16:08 +0100

    creating a bear post api/bears

--- server.js
+++ server.js
@@ -40,6 +40,26 @@ router.get('/', function (req, res) {
 
 // more routes for our API will happen here
 
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function (req, res) {
+
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function (err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear created!' });
+        });
+
+    });
+
 
 
 
